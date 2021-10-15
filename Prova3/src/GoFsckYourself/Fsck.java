fcd7b22dd4fbfd532a4ea6c591830fb6abb45d3f
Wellington Araújo <wellington.araujo.silva@ccc.ufcg.edu.br>
2021-10-15 16:54:23 -0300

    Correção do sistema de contagem

--- Prova3/src/GoFsckYourself/Fsck.java
+++ Prova3/src/GoFsckYourself/Fsck.java
@@ -1,7 +1,6 @@
 package GoFsckYourself;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 
 public class Fsck {
@@ -12,6 +11,8 @@ public class Fsck {
 	private int counterFsckedDirs = 0;
 	private int counterDamagedDirs = 0;
 	
+	Thread t1 = new Thread();
+	
 	public Fsck() {
 		currentDir = new File(System.getProperty("user.dir") + "/treeRoot");
 		displayDirectory(currentDir);
@@ -20,7 +21,7 @@ public class Fsck {
 	public boolean fsckDir(String dirPath) {
 		String[] dirPathSplited = dirPath.split("_");
 		System.out.println(Arrays.toString(dirPathSplited));
-		if(dirPathSplited.length > 1) {
+		if (dirPathSplited.length>1 && dirPathSplited[1] != null && dirPathSplited[1].equals("dmg")) {
 			return true;
 		}
 		return false;
@@ -34,59 +35,33 @@ public class Fsck {
 		}
 		return false;
 	}
-
-//	Ja existe uma funcao nativa na classe File file.listFiles();
-//	public List<File> readDir() {
-//		
-//	}
-//	
-//	//Ja existe uma funcao nativa na classe File file.isDirectory();
-//	public boolean isDir() {
-//		
-//	}
-//	
-//	public boolean fsckFile() {
-//		
-//	}
-//	
-	
-//
-//	Ja existe uma funcao nativa na classe File file.isParent();	
-//	public File parent() {
-//		
-//	}
 	
 	public void displayDirectory(File dir) {
-  
-        try {
-            File[] files = dir.listFiles();
-  
-            for (File file : files) {
-                if (file.isDirectory()) {
-                	counterFsckedDirs ++;
-                	if(fsckDir(file.getAbsolutePath())) {
-                		counterDamagedDirs ++;
-                		displayDirectory(file);
-                	}
-                    System.out.println("directory:" + file.getCanonicalPath());
-                    displayDirectory(file);
-                }
-                else {
-                    System.out.println("     file:" + file.getCanonicalPath());
-                }
-            }
-            
-//            for (File file : files) {
-//            	if (file.isDirectory()) {
-//            		displayDirectory(file);
-//            	}
-//            	
-//            }
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
+
+			File[] files = dir.listFiles();
+			
+			if (files != null) {
+				
+				for (File file : files) {
+					if (file.isDirectory()) {
+						counterFsckedDirs ++;
+						if(fsckDir(file.getAbsolutePath())) {
+							counterDamagedDirs ++;
+						}
+						displayDirectory(file);
+					} else if (fsckFile(file.getAbsolutePath())) {
+						counterFsckedFiles ++;
+						counterDamagedFiles ++;
+						if(fsckDir(file.getParent())) {
+							counterFsckedDirs ++;
+							counterDamagedDirs ++;
+							displayDirectory(file);
+						}
+					}
+
+				}
+			}
+			
        System.out.println("counterFsckedDirs: " + counterFsckedDirs + " counterDamagedDirs: " + counterDamagedDirs);
     }
 	
