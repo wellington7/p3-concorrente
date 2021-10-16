f2492f1713435d5a13a019412122a3c98d6b8252
Wellington Araújo <wellington.araujo.silva@ccc.ufcg.edu.br>
2021-10-16 17:24:46 -0300

    Update Main.java

--- Prova3/src/Main/Main.java
+++ Prova3/src/Main/Main.java
@@ -10,38 +10,43 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//*****TOKEN BUCKET*****
-		System.out.println("* TOKEN BUCKET - Iniciando a execucao");
+		// *****FSCK*****
+		System.out.println("");
+		System.out.println("* FSCK - Iniciando a execucao");
+		// System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		Fsck fsck = new Fsck();
+		new Thread() {
+			@Override
+			public void run() {
+				fsck.run();
+			}
+		}.start();
 		
+		// *****TOKEN BUCKET*****
+		System.out.println("");
+		System.out.println("* TOKEN BUCKET - Iniciando a execucao");
+
 		Integer bucketSize = 100;
 		Integer bucketTimeToAddToken = 100;
 		ArrayList<Request> requests = new ArrayList();
-		
-		// Gerando 100 requisicoes		
+
+		// Gerando 100 requisicoes
 		for (int i = 0; i < 20; i++) {
 			Request request = new Request(getRandomIntegerBetweenRange(bucketSize));
 			requests.add(request);
 		}
-		
+
 		Bucket bucket = new Bucket(bucketSize, bucketTimeToAddToken);
-		
-		
+
 		for (Request request : requests) {
 			new Thread() {
 				@Override
-			    public void run() {
+				public void run() {
 					bucket.run(request.getSize());
 				}
 			}.start();
 		}
-		
-		
-		
-//		//*****FSCK*****
-//		System.out.println("");
-//		System.out.println("* FSCK - Iniciando a execucao");
-//		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-//		Fsck fsck = new Fsck();
+			
 	}
 	
 	public static int getRandomIntegerBetweenRange(double max){
